628a1829cfe2d4d9ecec2bbe427b375c9ac753da
KKaged <dhigaredaleon707@gmail.com>
2023-04-25 17:44:04 +0900

    finished adding basic math functions and added files.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculator
+
+This will be a calculator site.
+
+You will be able to add, subtract, multiply and divide.
+
+Will be built using HTML, CSS and JS.
---
+++ script.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+  let result = a + b;
+  return result;
+}
+console.log(sum(1, 1));
+
+function sub(a, b) {
+  let result = a - b;
+  return result;
+}
+console.log(sub(6, 2));
+
+function multiply(a, b) {
+  let result = a * b;
+  return result;
+}
+console.log(multiply(2, 3));
+
+function divide(a, b) {
+  let result = a / b;
+  return result;
+}
+console.log(divide(16, 2));
